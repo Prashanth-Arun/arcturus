d8c8494c2933b6e272862abf9677c38098746a6a
Prashanth Arun <prashanth.arun@gmail.com>
2024-12-01 19:03:15 +0000

    Fix issues with BERT VAD classifier

--- system/components.py
+++ system/components.py
@@ -12,20 +12,24 @@ from torch.nn import Module, Linear, Dropout, MSELoss
 from typing import TypeVar
 from .unit import BERTForVADMappingOutput
 
+import torch
+
 DataConfig = TypeVar("DataConfig") # placeholder
 
 class BERTForVADMapping(Module):
 
     def __init__(self):
         super().__init__()
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-        self.model = BertModel.from_pretrained("bert-base-cased")
+        self.model = BertModel.from_pretrained("bert-base-cased").to(self.device)
         self.bert_config = self.model.config
         assert isinstance(self.bert_config, BertConfig)
 
         self.dropout = Dropout(self.bert_config.hidden_dropout_prob)
-        self.vad_head = Linear(self.bert_config.hidden_size, 3)
+        self.vad_head = Linear(self.bert_config.hidden_size, 3).to(self.device)
         self.loss_criterion = MSELoss()
+
 
     def forward(self, data : EmoBankData) -> BERTForVADMappingOutput:
 
@@ -35,7 +39,11 @@ class BERTForVADMapping(Module):
         B, T = data['input_ids'].shape
         E = self.bert_config.hidden_size
 
-        encoder_output : BaseModelOutput = self.model(**data)
+        encoder_output : BaseModelOutput = self.model(
+            input_ids=data['input_ids'],
+            attention_mask=data['attention_mask'],
+            token_type_ids=data['token_type_ids']
+        )
         hidden_state : Tensor = encoder_output.last_hidden_state[:, 0, :]
         assert hidden_state.shape == (B, E)
 
@@ -50,7 +58,6 @@ class BERTForVADMapping(Module):
             labels : Tensor = data['labels']
             assert labels.shape == (B, 3)
             loss : Tensor = self.loss_criterion(vad_embeddings, labels)
-            assert loss.shape == (B,)
             output['loss'] = loss
 
         return output
